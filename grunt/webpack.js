6d6801dffe797e80acb251759629c4f3b6ed27cb
Martin Sachse <martin.sachse+jsfi@gmail.com>
2015-12-12 11:30:45 +0100

    calculation htpassword

---
+++ grunt/webpack.js
@@ -0,0 +1,8 @@
+module.exports = {
+    htpasswd: {
+        entry: './src/views/templates/tool/htpasswd/htpasswd.js',
+        output: {
+            filename: 'build/tool/htpasswd/htpasswd.js'
+        }
+    }
+}
