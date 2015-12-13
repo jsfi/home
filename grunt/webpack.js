a5fce62edbadfddc71e8cc0efb0f2d44cac4e3d9
Martin Sachse <martin.sachse+jsfi@gmail.com>
2015-12-13 21:10:19 +0100

    added transpiler

--- grunt/webpack.js
+++ grunt/webpack.js
@@ -3,6 +3,12 @@ module.exports = {
         entry: './src/views/templates/tool/htpasswd/htpasswd.js',
         output: {
             filename: 'build/tool/htpasswd/htpasswd.js'
+        },
+        module: {
+            loaders: [{
+                test: /\.jsx?$/,
+                loader: 'babel'
+            }]
         }
     }
 }
